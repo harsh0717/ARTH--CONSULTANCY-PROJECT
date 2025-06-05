a3cb01ed2e776a546c87c9ff17bf4d49d7d6d10f
harsh170707 <153314900+harsh0717@users.noreply.github.com>
2025-06-05 13:35:44 +0530

    incomplete

--- main/app/login.php
+++ main/app/login.php
@@ -1,18 +1,31 @@
 <?php
 if (isset($_POST['user_name']) && isset($_POST['password'])) {
-    function validate_input($data) {
-	  $data = trim($data);
-	  $data = stripslashes($data);
-	  $data = htmlspecialchars($data);
-	  return $data;
+    include "../DB_connection.php";
+    function validate_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
     $user_name = validate_input($_POST['user_name']);
-	$password = validate_input($_POST['password']);
-    
-}
-else{
-    $em="unknown error occurred";
+    $password = validate_input($_POST['password']);
+
+    if (empty($user_name)) {
+        $em = "Please Enter User Name";
+        header("location: ../login.php?error=$em");
+        exit();
+    } elseif (empty($password)) {
+        $em = "Please Enter Password";
+        header("location: ../login.php?error=$em");
+        exit();
+    } else {
+        $sql = "SELECT * FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$user_name]);
+    }
+} else {
+    $em = "unknown error occurred";
     header("location: ../login.php?error=$em");
     exit();
 }
-?>
